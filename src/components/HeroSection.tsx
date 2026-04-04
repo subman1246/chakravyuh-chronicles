0f9665e3a2d3ce7f6d54f3c3b1a091f78242142d
Subman1246 <your_email@gmail.com>
2026-04-04 13:14:13 +0530

    fix hero background desktop

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           width: "100%",
           height: "100%",
           objectFit: "cover",
-          objectPosition: "top center",
+          objectPosition: "15% top",
         }}
       />
       {/* Overlay */}
